177e166a9f1d3814f99ba4379efbae2222861983
ruiyao <luoruiyao1@xiaomi.com>
2024-03-08 17:06:53 +0800

    commit

--- wgpu-tutorial/src/lib.rs
+++ wgpu-tutorial/src/lib.rs
@@ -7,6 +7,7 @@ use winit::{
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
 
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub fn run() {
   cfg_if::cfg_if! {
     if #[cfg(target_arch="wasm32")] {
@@ -18,13 +19,21 @@ pub fn run() {
   }
 
   // add canvas to the HTML document that we will host our application
-  // #[cfg(target_arch="wasm32")] {
-    // use winit::dpi::PhysicalSize;
-    // window.set_inner_size(PhysicalSize::new(450, 400));
+  #[cfg(target_arch="wasm32")] {
+    use winit::dpi::PhysicalSize;
+    window.set_inner_size(PhysicalSize::new(450, 400));
 
-    // use winit::platform::web::WindowExtWebSys;
-
-  // }
+    use winit::platform::web::WindowBuilderExtWebSys;
+    web_sys::window()
+      .and_then(|win| win.document())
+      .and_then(|doc| {
+        let dst = doc.get_element_by_id("wgpu-canvas").unwrap();
+        let canvas = web_sys::Element::from(window.canvas());
+        dst.append_child(&canvas).ok();
+        Some(())
+      })
+      .expect("couldn't add canvas to document");
+  }
 
   let event_loop = EventLoop::new().unwrap();
   let window = WindowBuilder::new().build(&event_loop).unwrap();
